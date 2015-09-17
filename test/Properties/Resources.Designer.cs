ad5437f0782e0a49ccc696d65180f721fd541607
Jaime Olivares <jolivares@ultralinq.com>
2015-09-17 02:39:40 -0500

    Added lib project and latest nuget package

--- test/Properties/Resources.Designer.cs
+++ test/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace ZipStorerTest.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap flash {
-            get {
-                object obj = ResourceManager.GetObject("flash", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
