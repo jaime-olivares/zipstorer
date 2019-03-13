2f4ee14439db71c6e2204b4fa525ed6dd4b552fd
David Tavarez <davidftavarez@gmail.com>
2019-03-13 09:19:58 -0400

    Update ZipStorer.cs
    missing var

--- src/ZipStorer.cs
+++ src/ZipStorer.cs
@@ -195,7 +195,7 @@ namespace System.IO.Compression
             
             /* prevent files/streams to be opened unused*/
             if(!_leaveOpen)
-                Close();
+                zip.Close();
             
             throw new System.IO.InvalidDataException();
         }
