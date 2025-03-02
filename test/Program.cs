3ca2b15d3c914542696ec845bd5304c23131bad1
Jaime Olivares <jolivares@efferenthealth.com>
2025-03-02 17:04:14 -0500

    Improved tests

--- test/Program.cs
+++ test/Program.cs
@@ -1,7 +1,7 @@
 using System;
+using System.Collections;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -24,15 +24,13 @@ namespace ZipStorerTest
         const string sampleFile4b = "sample4b.zip";
         const string sampleFile5 = "sample5.zip";
         const string sampleFile = "sample.zip";
-        private static byte[] buffer;
 
-        private readonly DateTime baseDate = new DateTime(2019,1,1);
+        private readonly DateTime baseDate = new DateTime(2019, 1, 1);
+        private readonly byte[] loremIpsum;
 
         public Program()
         {
-            string content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            buffer = Encoding.UTF8.GetBytes(content);
-
+            this.loremIpsum = File.ReadAllBytes("lorem_ipsum.txt");
         }
 
         // [TestMethod]
@@ -52,7 +50,7 @@ namespace ZipStorerTest
         public void OpenRead_Test()
         {
             using (ZipStorer zip = ZipStorer.Open(sampleFile, FileAccess.Read))
-            {                
+            {
             }
         }
 
@@ -75,7 +73,7 @@ namespace ZipStorerTest
                 Assert.IsFalse(dir.Count == 0);
                 zip.ExtractFile(dir[0], out byte[] output);
                 Assert.AreEqual(output.Length, 0);
-            }            
+            }
         }
 
         [TestMethod]
@@ -87,7 +85,7 @@ namespace ZipStorerTest
                 Assert.IsFalse(dir.Count == 0);
                 zip.ExtractFile(dir[4], out byte[] output);
                 Assert.IsFalse(output.Length == 0);
-            }            
+            }
         }
 
         [TestMethod]
@@ -97,7 +95,7 @@ namespace ZipStorerTest
             {
                 var dir = zip.ReadCentralDir();
                 Assert.IsTrue(dir[0].ModifyTime > baseDate);
-            }            
+            }
         }
 
         [TestMethod]
@@ -107,7 +105,7 @@ namespace ZipStorerTest
             {
                 var dir = zip.ReadCentralDir();
                 Assert.IsTrue(dir[0].AccessTime > DateTime.Today);
-            }            
+            }
         }
 
         [TestMethod]
@@ -118,15 +116,16 @@ namespace ZipStorerTest
                 var dir = zip.ReadCentralDir();
                 Assert.IsTrue(dir[0].CreationTime > baseDate);
                 Assert.IsTrue(dir[0].CreationTime <= dir[0].ModifyTime);
-            }            
+            }
         }
-        
+
         [TestMethod]
         public void CreateFile_Test()
         {
             File.Delete(sampleFile1);
 
-            using (ZipStorer zip = ZipStorer.Create(sampleFile1)) {};
+            using (ZipStorer zip = ZipStorer.Create(sampleFile1)) { }
+            ;
         }
 
         [TestMethod]
@@ -140,7 +139,7 @@ namespace ZipStorerTest
                 var dir = zip.ReadCentralDir();
                 Assert.IsFalse(dir.Count == 0);
                 Assert.IsTrue(dir[0].FilenameInZip == "Lorem.txt");
-            }            
+            }
         }
 
         [TestMethod]
@@ -157,7 +156,7 @@ namespace ZipStorerTest
                 Assert.IsTrue(dir[0].CreationTime >= now, "Creation Time failed");
                 Assert.IsTrue(dir[0].ModifyTime >= now, "Modify Time failed");
                 Assert.IsTrue(dir[0].AccessTime >= now, "Access Time failed");
-            }            
+            }
         }
 
         [TestMethod]
@@ -172,8 +171,8 @@ namespace ZipStorerTest
                 var dir = zip.ReadCentralDir();
                 Assert.IsFalse(dir.Count == 0);
                 Assert.IsTrue(dir[0].Method == ZipStorer.Compression.Deflate);
-                Assert.IsTrue(dir[0].CompressedSize < buffer.Length);
-            }            
+                Assert.IsTrue(dir[0].CompressedSize < loremIpsum.Length);
+            }
         }
 
         [TestMethod]
@@ -181,47 +180,47 @@ namespace ZipStorerTest
         {
             using (ZipStorer zip = ZipStorer.Create(sampleFile4a))
             {
-                using (var mem = new MemoryStream(buffer))
+                using (var mem = new MemoryStream(loremIpsum))
                 {
                     zip.AddStream(ZipStorer.Compression.Deflate, "Lorem1.txt", mem, baseDate);
                 }
-                using (var mem = new MemoryStream(buffer))
+
+                using (var mem = new MemoryStream(loremIpsum))
                 {
                     zip.AddStream(ZipStorer.Compression.Deflate, "Lorem2.txt", mem, baseDate);
                 }
-                using (var mem = new MemoryStream(buffer))
+
+                using (var mem = new MemoryStream(loremIpsum))
                 {
                     zip.AddStream(ZipStorer.Compression.Deflate, "Lorem3.txt", mem, baseDate);
                 }
             }
-            {
-                var zip = ZipStorer.Open(sampleFile4a, FileAccess.ReadWrite);
-                var entries = zip.ReadCentralDir();
-                var entry = entries[1];
-                ZipStorer.RemoveEntries(ref zip, new System.Collections.Generic.List<ZipStorer.ZipFileEntry> { entry });
-                zip.Close();
-            }
+
+            ZipStorer zip1 = ZipStorer.Open(sampleFile4a, FileAccess.ReadWrite);
+            var entries = zip1.ReadCentralDir();
+            var entry = entries[1];
+            ZipStorer.RemoveEntries(ref zip1, new System.Collections.Generic.List<ZipStorer.ZipFileEntry> { entry });
+            zip1.Close();
+
             using (ZipStorer zip = ZipStorer.Create(sampleFile4b))
             {
-                using (var mem = new MemoryStream(buffer))
+                using (var mem = new MemoryStream(loremIpsum))
                 {
                     zip.AddStream(ZipStorer.Compression.Deflate, "Lorem1.txt", mem, baseDate);
                 }
-                using (var mem = new MemoryStream(buffer))
+
+                using (var mem = new MemoryStream(loremIpsum))
                 {
                     zip.AddStream(ZipStorer.Compression.Deflate, "Lorem3.txt", mem, baseDate);
                 }
             }
+            
             using (var stream1 = new FileStream(sampleFile4a, FileMode.Open, FileAccess.Read))
-            using (var stream2 = new FileStream(sampleFile4b, FileMode.Open, FileAccess.Read))
             {
-                Assert.AreEqual(stream1.Length, stream2.Length);
-
-                var hash1 = MD5.HashData(stream1);
-                var hash2 = MD5.HashData(stream2);
-
-                Assert.AreEqual(hash1.Length, hash2.Length);
-                Assert.IsTrue(hash1.SequenceEqual(hash2));
+                using (var stream2 = new FileStream(sampleFile4b, FileMode.Open, FileAccess.Read))
+                {
+                    Assert.IsTrue(streamsAreEqual(stream1, stream2));
+                }
             }
         }
 
@@ -229,44 +228,48 @@ namespace ZipStorerTest
         public void Zip64_Test()
         {
             var dir = Path.Combine(Environment.CurrentDirectory, "SampleFiles5");
-            if (new DriveInfo(dir).AvailableFreeSpace < ((long)16496 * 1024 * 1024)) throw new Exception("Not enough disk space (16.1 GB) for test!");
-            if (Directory.Exists(dir)) Directory.Delete(dir, true);
-            if (Directory.Exists(dir + "_2")) Directory.Delete(dir + "_2", true);
+
+#if WINDOWS
+            if (new DriveInfo(dir).AvailableFreeSpace < ((long)16496 * 1024 * 1024)) 
+                throw new Exception("Not enough disk space (16.1 GB) for test!");
+#endif
+
+            if (Directory.Exists(dir)) 
+                Directory.Delete(dir, true);
+
+            if (Directory.Exists(dir + "_2")) 
+                Directory.Delete(dir + "_2", true);
+
             Directory.CreateDirectory(dir);
             Directory.CreateDirectory(dir + "_2");
             File.Delete(Path.Combine(dir, "..", sampleFile5));
 
-            // generate three test files
-            // standard text 
-            File.WriteAllBytes(Path.Combine(dir, "File1.txt"), buffer);
-            var txtBuffer = new byte[65538];
-            using (var mem = new MemoryStream(txtBuffer))
-            using (var bw = new BinaryWriter(mem, Encoding.ASCII))
-            {
-                for (int i = 0; i < 5958; i++)
-                {
-                    bw.Write(Encoding.ASCII.GetBytes("1234567890\n"));
-                }
-            }
-            // one larger than 0xFFFFFFFF and one with 0xFFFFFFFE bytes
+            // Create one test file larger than 0xFFFFFFFF and one with 0xFFFFFFFE bytes
             for (int n = 2; n <= 3; n++)
             {
                 using (var fs = new FileStream(Path.Combine(dir, $"File{n}.txt"), FileMode.Create))
                 {
-                    for (var i = 0; i < (n == 2 ? 66000 : 65534); i++)
+                    int max = (int)Math.Floor(0x100000000L / (float)this.loremIpsum.Length) + (n==2 ? 1 : 0);
+
+                    for (var i = 0; i < max; i++)
                     {
-                        fs.Write(txtBuffer, 0, txtBuffer.Length);
+                        fs.Write(loremIpsum.AsSpan());
                     }
-                    if (n == 3) fs.Write(txtBuffer, 0, 2);
+
+                    if (n == 3) 
+                    {
+                        int remainder = (int)(0xFFFFFFFE - fs.Length);
+                        fs.Write(this.loremIpsum, 0, remainder);
+                    }
                 }
             }
 
             // zip them
             using (ZipStorer zip = ZipStorer.Create(Path.Combine(dir, "..", sampleFile5)))
             {
-                zip.AddFile(ZipStorer.Compression.Deflate, Path.Combine(dir, "File1.txt"), "File1.txt");  // normal file
+                zip.AddFile(ZipStorer.Compression.Deflate, Path.Combine(dir, "lorem_ipsum.txt"), "File1.txt");  // normal file
                 zip.AddFile(ZipStorer.Compression.Deflate, Path.Combine(dir, "File2.txt"), "File2.txt");  // Zip64 file size, normal compressed size and offset
-                zip.AddFile(ZipStorer.Compression.Store, Path.Combine(dir, "File3.txt"), "File3.txt");    // normal file size and offset
+                zip.AddFile(ZipStorer.Compression.Store,   Path.Combine(dir, "File3.txt"), "File3.txt");  // normal file size and offset
                 zip.AddFile(ZipStorer.Compression.Deflate, Path.Combine(dir, "File2.txt"), "File4.txt");  // Zip64 file size and offset
             }
 
@@ -274,46 +277,44 @@ namespace ZipStorerTest
             using (ZipStorer zip = ZipStorer.Open(Path.Combine(dir, "..", sampleFile5), FileAccess.Read))
             {
                 var entries = zip.ReadCentralDir();
+
                 for (var n = 0; n < 4; n++)
                 {
                     zip.ExtractFile(entries[n], Path.Combine(dir + "_2", entries[n].FilenameInZip));
+
                     using (var fs1 = new FileStream(Path.Combine(dir, entries[n == 3 ? 1 : n].FilenameInZip), FileMode.Open))
-                    using (var fs2 = new FileStream(Path.Combine(dir + "_2", entries[n].FilenameInZip), FileMode.Open))
                     {
-                        Assert.IsTrue(StreamsAreEqual(fs1, fs2));
+                        using (var fs2 = new FileStream(Path.Combine(dir + "_2", entries[n].FilenameInZip), FileMode.Open))
+                        {
+                            Assert.IsTrue(streamsAreEqual(fs1, fs2));
+                        }
                     }
+
                     File.Delete(Path.Combine(dir + "_2", entries[n].FilenameInZip));
                 }
             }
         }
 
-        private bool StreamsAreEqual(Stream s1, Stream s2)
+        private bool streamsAreEqual(Stream s1, Stream s2)
         {
-            if (s1.Length != s2.Length) return false;
+            using var sha256 = SHA256.Create();
+            byte[] hash1 = sha256.ComputeHash(s1);
+            byte[] hash2 = sha256.ComputeHash(s2);
 
-            var bytes1 = new byte[65536];
-            var bytes2 = new byte[65536];
-            long bytesLeft = s1.Length;
-            while (bytesLeft > 0)
-            {
-                var bytesRead1 = s1.Read(bytes1, 0, bytes1.Length);
-                var bytesRead2 = s2.Read(bytes2, 0, bytes2.Length);
-                if (!bytes1.SequenceEqual(bytes2)) return false;
-                bytesLeft -= bytesRead1;
-            }
-            return true;
+            return StructuralComparisons.StructuralEqualityComparer.Equals(hash1, hash2);
         }
 
-        public void createSampleFile()
+        private void createSampleFile()
         {
-            using (var mem = new MemoryStream(buffer))
+            using (var mem = new MemoryStream(this.loremIpsum))
             {
                 File.Delete(sampleFile1);
+                
                 using (ZipStorer zip = ZipStorer.Create(sampleFile1))
                 {
                     zip.AddStream(ZipStorer.Compression.Deflate, "Lorem.txt", mem, DateTime.Now);
                 }
             }
-        }        
+        }
     }
 }
